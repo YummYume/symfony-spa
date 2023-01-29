b3307bb36eb175c6df050e78e56cad9618850ae2
LeoBruant <bruantleo@gmail.com>
2023-01-29 21:15:49 +0100

    Added homepage style

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Manager\FlashManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,5 +14,14 @@ final class HomepageController extends AbstractController
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig');
+    }
+
+    #[Route('/test', name: 'app_flash-test', methods: ['GET'])]
+    public function flashTest(FlashManager $flashManager): RedirectResponse
+    {
+        $flashManager->flash(FlashManager::FLASH_SUCCESS, 'Test success', ['type' => FlashManager::FLASH_SUCCESS]);
+        $flashManager->flash(FlashManager::FLASH_ERROR, 'Test error');
+
+        return $this->redirectToRoute('app_homepage');
     }
 }
