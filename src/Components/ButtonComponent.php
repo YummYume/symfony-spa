cbf888d313cb6487ce02243bb42adafd98198fea
LeoBruant <bruantleo@gmail.com>
2023-02-04 18:08:15 +0100

    Improved button component

--- src/Components/ButtonComponent.php
+++ src/Components/ButtonComponent.php
@@ -8,6 +8,9 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent('button')]
 final class ButtonComponent
 {
+    public const LEFT = 'left';
+    public const RIGHT = 'right';
+
     public bool $centered = true;
     public bool $circle = false;
     public bool $ghost = false;
@@ -15,5 +18,7 @@ final class ButtonComponent
     public bool $submit = false;
     public string $class = '';
     public string $content = '';
+    public ?string $icon = null;
+    public string $iconPosition = self::RIGHT;
     public string $variant = ColorTypeEnum::PRIMARY->value;
 }
