86f6b096cc2b5c8141ae7e9083fe1400bcd1ad55
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-02-04 20:08:32 +0000

    fix: lint theme controller

--- assets/app/controllers/theme_controller.ts
+++ assets/app/controllers/theme_controller.ts
@@ -1,5 +1,6 @@
-import { TurboBeforeVisitEvent } from '@hotwired/turbo';
-import { Context, Controller } from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus';
+
+import type { TurboBeforeVisitEvent } from '@hotwired/turbo';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
@@ -13,10 +14,6 @@ export default class extends Controller {
 
   declare readonly lightTarget: HTMLElement;
 
-  constructor(context: Context) {
-    super(context);
-  }
-
   connect() {
     this.element.addEventListener('turbo:before-visit', this.setTheme.bind(this));
   }
@@ -25,7 +22,7 @@ export default class extends Controller {
     this.element.removeEventListener('turbo:before-visit', this.setTheme.bind(this));
   }
 
-  private setTheme(e: TurboBeforeVisitEvent) {
+  private setTheme(e: TurboBeforeVisitEvent): void {
     e.preventDefault();
 
     const urlLength = e.detail.url.split('/').length;
@@ -43,15 +40,16 @@ export default class extends Controller {
     }
 
     if (this.hasLightTarget && this.hasDarkTarget) {
-      switch (mode) {
-        case 'dark':
-          this.lightTarget.classList.contains('swap-on') ? this.lightTarget.classList.replace('swap-on', 'swap-off') : null;
-          this.darkTarget.classList.contains('swap-off') ? this.darkTarget.classList.replace('swap-off', 'swap-on') : null;
-          break;
-        case 'light':
-          this.lightTarget.classList.contains('swap-off') ? this.lightTarget.classList.replace('swap-off', 'swap-on') : null;
-          this.darkTarget.classList.contains('swap-on') ? this.darkTarget.classList.replace('swap-on', 'swap-off') : null;
-          break;
+      if (mode === 'dark') {
+        if (this.lightTarget.classList.contains('swap-on')) this.lightTarget.classList.replace('swap-on', 'swap-off');
+
+        if (this.darkTarget.classList.contains('swap-off')) this.darkTarget.classList.replace('swap-off', 'swap-on');
+      }
+
+      if (mode === 'light') {
+        if (this.lightTarget.classList.contains('swap-off')) this.lightTarget.classList.replace('swap-off', 'swap-on');
+
+        if (this.darkTarget.classList.contains('swap-on')) this.darkTarget.classList.replace('swap-on', 'swap-off');
       }
     }
   }
@@ -59,14 +57,15 @@ export default class extends Controller {
   private getCookie(name: string): string | null {
     const nameEQ = `${name}=`;
     const cookies = document.cookie.split(';');
-    let result: string|null = null;
+    let result: string | null = null;
 
-    cookies.forEach((cookie) => {
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1, cookie.length);
+    cookies.forEach((cookie: string) => {
+      let ck: string = cookie;
+      while (ck.charAt(0) === ' ') {
+        ck = ck.substring(1, ck.length);
       }
-      if (cookie.indexOf(nameEQ) === 0) {
-        result = cookie.substring(nameEQ.length, cookie.length);
+      if (ck.indexOf(nameEQ) === 0) {
+        result = ck.substring(nameEQ.length, ck.length);
       }
     });
 
