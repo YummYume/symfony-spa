e5fa65bcd9b59960554d57945416003321b8a9e6
YummYume <yummyume@users.noreply.github.com>
2023-02-04 18:39:44 +0100

    Patch gitignore and migration description

--- migrations/Version20230203153952.php
+++ migrations/Version20230203153952.php
@@ -14,7 +14,7 @@ final class Version20230203153952 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add initial tables.';
     }
 
     public function up(Schema $schema): void
