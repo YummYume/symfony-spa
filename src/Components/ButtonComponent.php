53a7e258cd2b26264468d0f05ad63e63abd55ef9
LeoBruant <bruantleo@gmail.com>
2023-02-04 18:51:36 +0100

    Fix: Added new lines

--- src/Components/ButtonComponent.php
+++ src/Components/ButtonComponent.php
@@ -9,16 +9,26 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 final class ButtonComponent
 {
     public const LEFT = 'left';
+
     public const RIGHT = 'right';
 
     public bool $centered = true;
+
     public bool $circle = false;
+
     public bool $ghost = false;
+
     public bool $square = false;
+
     public bool $submit = false;
+
     public string $class = '';
+
     public string $content = '';
+
     public ?string $icon = null;
+
     public string $iconPosition = self::RIGHT;
+
     public string $variant = ColorTypeEnum::PRIMARY->value;
 }
