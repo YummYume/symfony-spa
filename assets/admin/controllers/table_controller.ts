37d0feac834ac7a9ba8fbbd780e7bafde1e7bc7a
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-02-09 18:00:41 +0000

    fix: lint

--- assets/admin/controllers/table_controller.ts
+++ assets/admin/controllers/table_controller.ts
@@ -12,24 +12,25 @@ export default class extends Controller {
 
   declare readonly messageTarget: HTMLElement;
 
-  private async switch(e: any): Promise<void> {
-    await fetch(`${window.origin}${e.target?.dataset.route}`);
+  private async switch(e: Event): Promise<void> {
+    const target = e.target as HTMLElement;
+    if (target.dataset.route) {
+      await fetch(`${window.origin}${target.dataset.route}`);
+    }
   }
 
   private action(e: Event) {
     const target = e.target as HTMLElement;
-    let el: HTMLElement|null = target;
-    
-    if(target.localName === 'svg') {
+    let el: HTMLElement | null = target;
+    if (target.localName === 'svg') {
       el = target.parentElement;
     }
 
-    if(el) {
+    if (el) {
       const { route, message } = el.dataset;
       if (message) {
         this.messageTarget.textContent = message;
       }
-  
       if (route) {
         this.routeTarget.href = (`${window.origin}${route}`);
       }
