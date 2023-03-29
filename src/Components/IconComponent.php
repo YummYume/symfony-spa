424c106812fdf999f8d52cd2335a69f33d7499af
Leo <bruantleo@gmail.com>
2023-03-29 15:59:14 +0200

    Fix : Pagination

--- src/Components/IconComponent.php
+++ src/Components/IconComponent.php
@@ -7,7 +7,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent('icon')]
 class IconComponent
 {
-    public string $class = 'h-6 w-6';
+    public string $class = 'h-6 w-6h-6 w-6';
 
     public ?string $icon = null;
 }
